52e6fa25dc82ea74dfb8cb42e0611a49b7ee0124
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-07-08 13:47:12 +0900

    react memo

--- src/atom.tsx
+++ src/atom.tsx
@@ -1,39 +1,6 @@
 import { atom, selector } from "recoil";
-import { recoilPersist } from "recoil-persist";
 
-const { persistAtom } = recoilPersist({
-  key: "todoLocal",
-  storage: localStorage,
-});
-
-export enum Categories {
-  "TODO" = "TODO",
-  "DOING" = "DOING",
-  "DONE" = "DONE",
-}
-
-export interface IToDo {
-  text: string;
-  category: Categories;
-  id: number;
-}
-
-export const categoryState = atom<Categories>({
-  key: "category",
-  default: Categories.TODO,
-});
-
-export const toDoState = atom<IToDo[]>({
+export const toDoState = atom({
   key: "toDo",
-  default: [],
-  effects_UNSTABLE: [persistAtom],
-});
-
-export const toDoSelector = selector({
-  key: "toDoSelector",
-  get: ({ get }) => {
-    const toDos = get(toDoState);
-    const category = get(categoryState);
-    return toDos.filter((toDo) => toDo.category === category);
-  },
+  default: ["a", "b", "c", "d", "e", "f"],
 });
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from "./App";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
 import { darkTheme } from "./theme";
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
